651e38c2f667009d16fe2ef9039253b6c58fcb32
Hugo Lara <hugo.alex200197@outlook.com>
2021-06-24 13:59:26 -0500

    Cracion de src exportacion de randomMsg

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const messages = [
+  "Oscar",
+  "Karen",
+  "Hugo",
+  "Diego"
+];
+
+const randomMsg = () => {
+  const message = messages[Math.floor(Math.random()* messages.length)];
+  console.log(message);
+};
+
+module.exports = {randomMsg};
